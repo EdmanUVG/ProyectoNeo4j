d26dbed78a7284536862600242a25ec7c87917b2
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2020-05-06 22:17:25 -0600

    Update Turismo.py

--- Neo4j/Turismo.py
+++ Neo4j/Turismo.py
@@ -14,6 +14,8 @@ class Turismo:
     def close(self):
         self.driver.close()
 
+
+
     # Envia el codigo del metodo '_create_person' a la base de datos.
     def save_name(self, name, age):
         with self.driver.session() as session:
@@ -22,6 +24,14 @@ class Turismo:
     def save_place(self, place_name, location):
         with self.driver.session() as session_save_place:
             session_save_place.write_transaction(self._create_place, place_name, location)
+
+    def find_user(self, username):
+        with self.driver.session() as session_find_user:
+            session_find_user.read_transaction(self._search_user, username)
+
+    # def load_user_preference(self, preference):
+    #     with self.driver.session() as session_load_user_preference:
+    #         session_load_user_preference.read_transaction(self._search_user_preference, preference)
 
     # Implementa el lenguaje Cypher para crear un nodo con etiqueta Person
     # con atributos 'name y age '
