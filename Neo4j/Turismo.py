fea5ac848ddba7c76f76dbbab55bcb75616422c6
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2020-05-06 22:15:30 -0600

    Update Turismo.py

--- Neo4j/Turismo.py
+++ Neo4j/Turismo.py
@@ -1,7 +1,12 @@
-from neo4j import GraphDatabase
+import sys
 
+from neo4j import GraphDatabase
+import os
+from sys import platform
+import time
 
 class Turismo:
+
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password), encrypted=False)
